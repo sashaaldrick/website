bee56b6dd1609074ac98b913d1af4e1d37ef6537
Sasha Aldrick <sasha@aldrick.dev>
2025-02-19 18:06:47 -0700

    fix annoying config

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			// Build directly to the directory Cloudflare Pages expects
+			outDir: 'build'
+		}),
 		alias: {
 			$lib: './src/lib'
 		}
